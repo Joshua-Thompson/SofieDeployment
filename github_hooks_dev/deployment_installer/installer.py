f319e967b4aeae015a0263c5186bc55a0c513930
jcatterson <justin.catterson@gmail.com>
2015-11-18 12:54:37 -0800

    Logging occurs to standard output and to the Applications label

--- github_hooks_dev/deployment_installer/installer.py
+++ github_hooks_dev/deployment_installer/installer.py
@@ -1,28 +1,59 @@
 #!/usr/bin/python
-
 import sys
+import logging
+import threading
+
 from PyQt4 import QtGui, QtCore
 import updater
+
+logger = logging.getLogger("installer")
+logger.setLevel(logging.DEBUG)
+
+class InstallHandler(logging.Handler):
+    def __init__(self,level=logging.NOTSET):
+        logging.Handler.__init__(self,level)
+        self.qtText = None
+
+    def handle(self, record):
+        if self.qtText:
+            self.qtText.setText(record.msg)
+
+    def setLoggerText(self, qtText):
+        self.qtText = qtText
+
+hdlr = InstallHandler()
+logger.addHandler(hdlr)
 
 class ElixysInstaller(QtGui.QMainWindow):
     
     def __init__(self):
         super(ElixysInstaller, self).__init__()
         self.initUI()
-        
+        hdlr.setLoggerText(self.status_label)
+        self.show()
+
     def initUI(self):
         self.setWindowTitle('Elixys Installer')
         self.mainWidget=QtGui.QWidget(self)
-        self.setCentralWidget(self.mainWidget)
-        self.button = QtGui.QPushButton('Select Elixys Install',self.mainWidget)
+        layout = QtGui.QVBoxLayout()
+        self.button = QtGui.QPushButton('Select Elixys Install',self)
+        self.status_label = QtGui.QLabel("", self)
+
+        layout.addWidget(self.button)
+        layout.addWidget(self.status_label)
         self.button.clicked.connect(self.install_elixys_version)
-        self.show()
+        widget = QtGui.QWidget()
+        widget.setLayout(layout)
+        self.setCentralWidget(widget)
+
         
     def install_elixys_version(self):
         fname = QtGui.QFileDialog.getOpenFileName(self, 'Install',
                 '.')
+
         if fname:
-            updater.do_install(fname)
+            t = threading.Thread(target=updater.do_install, args=(fname,))
+            t.start()
 
 def main():
     app = QtGui.QApplication(sys.argv)
