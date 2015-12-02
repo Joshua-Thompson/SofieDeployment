5a5ad918ff6627e32c073971e41ce927a4cda82e
jcatterson <justin.catterson@gmail.com>
2015-12-01 17:28:55 -0800

    WebViewer is beginning to work

--- github_hooks_dev/deployment_installer/setup.sh
+++ github_hooks_dev/deployment_installer/setup.sh
@@ -1,18 +1,26 @@
 #!/bin/bash
 
+installSip(){
+    wget http://sourceforge.net/projects/pyqt/files/sip/sip-4.17/sip-4.17.tar.gz
+    tar -xvf sip-4.17.tar.gz
+    cd sip-4.17/
+    python configure.py -e ../include/
+    sudo make install
+    cd ../
+}
+
+installPyQt(){
+    wget http://sourceforge.net/projects/pyqt/files/PyQt5/PyQt-5.5.1/PyQt-gpl-5.5.1.tar.gz
+    tar -xvf PyQt-gpl-5.5.1.tar.gz
+    cd PyQt-gpl-5.5.1
+    python configure.py
+    make
+    make install
+    cd ../
+}
+
 virtualenv .
 source bin/activate
-wget http://sourceforge.net/projects/pyqt/files/PyQt4/PyQt-4.11.4/PyQt-x11-gpl-4.11.4.tar.gz
-wget http://sourceforge.net/projects/pyqt/files/sip/sip-4.17/sip-4.17.tar.gz
-tar -xvf sip-4.17.tar.gz
-cd sip-4.17/
-python configure.py -e ../include/python2.7/
-sudo make install
-cd ../
-tar -xvf PyQt-x11-gpl-4.11.4.tar.gz
-cd PyQt-x11-gpl-4.11.4
-python configure.py
-make
-make install
-cd ../
+installSip
+installPyQt
 pip install -r requirements.txt
---
+++ github_hooks_dev/deployment_installer/ui_controllers/webview.py
@@ -0,0 +1,52 @@
+import sys
+from PyQt5.QtWidgets import QApplication
+from PyQt5 import QtCore, uic
+from PyQt5.QtWebEngineWidgets import QWebEngineView, QWebEngineProfile, QWebEnginePage
+
+class NetworkApp(QWebEngineView):
+    def __init__(self):
+        super(NetworkApp, self).__init__()
+        self.profile = QWebEngineProfile("network_history")
+        self.download = None
+        self.profile.setPersistentCookiesPolicy(QWebEngineProfile.ForcePersistentCookies)
+        self.profile.downloadRequested.connect(self.downloading)
+        self._page = QWebEnginePage(self.profile, self)
+        self._page.loadStarted.connect(self.load_started)
+        self._page.load(QtCore.QUrl("http://sofiebio.herokuapp.com/"))
+        self._page.loadProgress.connect(self.load_progress)
+        self.setPage(self._page)
+
+    def load_started(self):
+        print "Loading..."
+
+    def load_progress(self,val):
+        print "Progress %i" % val
+
+    def download_complete(self):
+        print 'Download Complete'
+
+    def download_in_progress(self, bytesReceived, bytesTotal):
+        print "Downloading ... %d/%d" % (bytesReceived,bytesTotal)
+
+    def downloading(self, down):
+        self.download = down
+        self.download.finished.connect(self.download_complete)
+        self.download.downloadProgress.connect(self.download_in_progress)
+        self.download.accept()
+
+    def closeEvent(self, evt):
+        self.deleteLater()
+        self._page.deleteLater()
+        self.profile.deleteLater()
+        evt.accept()
+
+def main():
+    app = QApplication(sys.argv)
+    view = NetworkApp()
+    view.show()
+    exec_status = app.exec_()
+    sys.exit(exec_status)
+
+
+if __name__ == '__main__':
+    main()
