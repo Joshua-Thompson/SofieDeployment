897a8c6c92df7345af30d80dcd5ba58f9b677e1a
Justin Catterson <Justin Catterson>
2015-12-16 14:02:03 -0800

    The Installer is working properly

--- github_hooks_dev/deployment_installer/windows_exe_builder.bat
+++ github_hooks_dev/deployment_installer/windows_exe_builder.bat
@@ -1,4 +1,2 @@
 pyinstaller -y --windowed installer.spec
-xcopy C:\Qt\5.5\msvc2013\bin\QtWebEngineProcess.exe dist\installer
-xcopy C:\Qt\5.5\msvc2013\*.* dist\installer
-xcopy qt.conf dist\installer
+
